35b3c0792adfc4e61659a50d28c26a99312aa67e
Mahmoud Elewa <m.2020.elewa@gmail.com>
2020-12-26 09:25:21 +0200

    update animations for dashboard and welcome pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,15 @@
     <!-- Masthead-->
     <section class="masthead d-flex align-items-center justify-content-center">
         <div class="container col-md-6 align-items-center justify-content-center text-center">
-            <div class="animate__animated animate__bounceInDown">
-            <h1 class="text-uppercase text-white font-weight-bold pb-3">Vurtify - Boilerplate Project</h1>
-            <p class="text-white-75 font-weight-light mb-5">
-                Get the best out of Laravel 8 - without dealing with the complexity of Jetstream and Tailwind. Instead, we are using Bootstrap 5 and Vue 3
-            </p>
-        </div>
-            <a class="btn btn-primary btn-xl text-white" href="{{ route('register') }}">Learn more</a>
+            <div class="animate__animated animate__bounceInDown animate__fast">
+                <h1 class="text-uppercase text-white font-weight-bold pb-3">Vurtify - Boilerplate Project</h1>
+                <p class="text-white-75 font-weight-light mb-5">
+                    Get the best out of Laravel 8 - without dealing with the complexity of Jetstream and Tailwind.
+                    Instead, we are using Bootstrap 5 and Vue 3
+                </p>
+            </div>
+            <a class="btn btn-primary btn-xl text-white animate__animated animate__bounceInUp animate__fast"
+                href="{{ route('register') }}">Learn more</a>
         </div>
     </section>
 </x-dynamic-component>
